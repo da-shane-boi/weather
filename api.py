04a081c4844a0a5599dae78b0c6807a4b16652b3
scarr023 <scarr023@student.wethinkcode.co.za>
2024-03-17 10:22:23 +0200

    added astrological data

--- api.py
+++ api.py
@@ -1,17 +1,23 @@
 from pathlib import Path
 import requests
 import os
-import json
+import util
+
 
 class Api:
     def __init__(self) -> None:
         self.base_url = "https://api.weatherapi.com/v1"
-        self.path = Path.home() / ".weather"
+        self.path = Path.home() / ".config" / "weather"
         self.KEY = self.configure()
 
-        self.current_response = None
+        self.response = None
 
     def configure(self) -> str:
+        """Configures the Api, acquiring the key and saving it to a local file.
+
+        Returns:
+            str: Api key
+        """
         if self.path.exists():
             key_path = self.path / ".key.txt"
             if key_path.exists():
@@ -30,6 +36,11 @@ class Api:
         return key
 
     def save_key(self, key):
+        """Saves the key in a hidden file in the home directory
+
+        Args:
+            key (_type_): _description_
+        """
         os.system(f"echo '{key}' > {self.path}/.key.txt")
 
     def get_key(self) -> str:
@@ -43,44 +54,61 @@ class Api:
         response = requests.get(f"{self.base_url}/current.json", params=paramaters)
         return str(response.status_code)[0] != "4"
 
-    # def create_config(self):
-    #     config_path = self.path/"config.json"
-    #     if not config_path.exists:
-    #         Path.touch(self.path/"config.json")
-    #         # default location,
+    def get(self, type: str, paramaters: dict):
+        self.response = requests.get(
+            f"{self.base_url}/{type}.json", params=paramaters
+        ).json()
 
-    def get(self, type: str, paramaters:dict):
-        # paramaters = {"key": self.KEY, "q": location}
-        self.response = requests.get(f"{self.base_url}/{type}.json", params=paramaters).json()
+    def print_response_current(self, json:bool):
+        if json:
+            print(self.response)
+        else:
+            print("_________________________________________________________________")
+            print()
+            print(f"Location:          {self.response['location']['name']}")
+            print(f"Condition:         {self.response['current']['condition']['text']}")
+            print(f"Temperature:       {self.response['current']['temp_c']}°C")
+            if self.response['current']['temp_c'] != self.response['current']["feelslike_c"]:
+                print(f"    Feels like:    {self.response['current']['feelslike_c']}°C")
+            print(f"Humidity:          {self.response['current']['humidity']}%")
+            if self.response["current"]["precip_mm"] > 0.1:
+                print(f"Rain:              {self.response['current']['precip_mm']}mm")
+            print(
+                f"Wind:              {self.response['current']['wind_kph']}Km {self.response['current']['wind_dir']}"
+            )
+            print(f"UV Index:          {util.get_uv_index_rate(self.response['current']['uv'])}")
+            print("_________________________________________________________________")
 
-    def print_response_current(self):
-        print("_________________________________________________________________")
-        print()
-        print(f"Location:          {self.response['location']['name']}")
-        print(f"Condition:         {self.response['current']['condition']['text']}")
-        print(f"Temperature:       {self.response['current']['temp_c']}°C")
-        print(f"    Feels like:    {self.response['current']['feelslike_c']}°C")
-        print(f"Humidity:          {self.response['current']['humidity']}%")
-        if self.response['current']['precip_mm'] > 0.1:
-            print(f"Rain:              {self.response['current']['precip_mm']}mm")
-        print(f"Wind:              {self.response['current']['wind_kph']}Km {self.response['current']['wind_dir']}")
-        # print(f"Wind Direction: {self.response['current']['wind_dir']}")
-        print("_________________________________________________________________")
+    def print_response_forecast(self, json:bool, all:bool):
+        fore_day = self.response["forecast"]["forecastday"][-1]
+        if json:
+            print(self.response)
+        else:
+            print("_________________________________________________________________")
+            print()
+            print(f"Location:          {self.response['location']['name']}")
+            print(
+                f"Date:              {util.get_day(fore_day['date'])}, {fore_day['date'].split('-')[2]} {util.get_month(fore_day['date'])}"
+            )
+            print(f"Condition:         {fore_day['day']['condition']['text']}")
+            print(
+                f"Temperature:       {fore_day['day']['mintemp_c']}°C/{fore_day['day']['maxtemp_c']}°C"
+            )
+            print(f"Humidity:          {fore_day['day']['avghumidity']}%")
+            print(f"Chance of rain:    {fore_day['day']['daily_chance_of_rain']}%")
+            print(f"Chance of snow:    {fore_day['day']['daily_chance_of_snow']}%")
+            print(f"Wind:              {fore_day['day']['maxwind_kph']}Km")
+            print(f"UV Index:          {util.get_uv_index_rate(fore_day['day']['uv'])}")
+            print("_________________________________________________________________")
+            if all:
+                print()
+                print(f"Sunrise:           {fore_day['astro']['sunrise']}")
+                print(f"Sunset:            {fore_day['astro']['sunset']}")
+                print(f"Moonrise:          {fore_day['astro']['moonrise']}")
+                print(f"Moonset:           {fore_day['astro']['moonset']}")
+                print(f"Moon Phase:        {fore_day['astro']['moon_phase']}")
+                print(f"Moon Light:        {fore_day['astro']['moon_illumination']}")
+                print("_________________________________________________________________")
 
-    def print_response_forecast(self):
-        fore_day = self.response['forecast']['forecastday'][-1]
-        # print(fore_day)
-        print("_________________________________________________________________")
-        print()
-        print(f"Location:          {self.response['location']['name']}")
-        print(f"Date:              {'-'.join(fore_day['date'].split('-')[::-1])}")
-        print(f"Condition:         {fore_day['day']['condition']['text']}")
-        print(f"Temperature:       {fore_day['day']['mintemp_c']}°C/{fore_day['day']['maxtemp_c']}°C")
-        print(f"Humidity:          {fore_day['day']['avghumidity']}%")
-        print(f"Chance of rain:    {fore_day['day']['daily_chance_of_rain']}%")
-        print(f"Wind:              {fore_day['day']['maxwind_kph']}Km")
-        print(f"UV Index:          {fore_day['day']['uv']}")
-        # # print(f"Wind Direction: {self.response['current']['wind_dir']}")
-        print("_________________________________________________________________")
 
-
+            
--- main.py
+++ main.py
@@ -12,17 +12,38 @@ def create_parser():
         "--forecast",
         metavar="days",
         type=int,
-        help="Input the amount of days that you would like to forecast, max of 3",
+        choices=range(1, 5),
+        help="Input the amount of days that you would like to forecast, max of 4",
     )
     parser.add_argument(
         "location",
         nargs="?",
-        # const="Cape Town",
         default="Cape Town",
         metavar='"location"',
         type=str,
         help="Put in a location you would like to get the weather for.",
     )
+
+    parser.add_argument(
+        "-t", "--tomorrow", action="store_true", help="Get the weather for tomorrow"
+    )
+
+    parser.add_argument(
+        "-json",
+        "--get_json",
+        action="store_true",
+        default=False,
+        help="Get Api request response json",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        default=False,
+        help="See all data for forecast"
+    )
+
     return parser
 
 
@@ -31,15 +52,17 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
     api = Api()
-
+    json = True if args.get_json else False
+    all = True if args.all else False
     if args.forecast:
         params = {"key": api.KEY, "q": args.location, "days": args.forecast}
         api.get("forecast", params)
-        api.print_response_forecast()
+        api.print_response_forecast(json, all)
+    elif args.tomorrow:
+        params = {"key": api.KEY, "q": args.location, "days": 2}
+        api.get("forecast", params)
+        api.print_response_forecast(json, all)
     else:
         params = {"key": api.KEY, "q": args.location}
         api.get("current", params)
-        api.print_response_current()
-
-    # api.get("current")
-    # api.print_response()
+        api.print_response_current(json)
